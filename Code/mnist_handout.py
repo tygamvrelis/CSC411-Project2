bdcc909859c0e7343b28112d12c8fc4d01066e36
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-02-05 00:07:14 -0500

    Part 2 write-up

--- Code/mnist_handout.py
+++ Code/mnist_handout.py
@@ -42,9 +42,12 @@ for k in M.keys():
         M[k] = temp
 
 def softmax(y):
-    '''Return the output of the softmax function for the matrix of output y. y
+    '''
+    Return the output of the softmax function for the matrix of output y. y
     is an NxM matrix where N is the number of outputs for a single case, and M
-    is the number of cases'''
+    is the number of cases
+    '''
+    
     return exp(y)/tile(sum(exp(y),0), (len(y),1))
     
 def Part2(theta, X):
@@ -62,9 +65,12 @@ def Part2(theta, X):
 
 ## Others
 def tanh_layer(y, W, b):    
-    '''Return the output of a tanh layer for the input matrix y. y
+    '''
+    Return the output of a tanh layer for the input matrix y. y
     is an NxM matrix where N is the number of inputs for a single case, and M
-    is the number of cases'''
+    is the number of cases
+    '''
+    
     return tanh(dot(W.T, y)+b)
 
 def forward(x, W0, b0, W1, b1):
@@ -77,8 +83,11 @@ def NLL(y, y_):
     return -sum(y_*log(y)) 
 
 def deriv_multilayer(W0, b0, W1, b1, x, L0, L1, y, y_):
-    '''Incomplete function for computing the gradient of the cross-entropy
-    cost function w.r.t the parameters of a neural network'''
+    '''
+    Incomplete function for computing the gradient of the cross-entropy
+    cost function w.r.t the parameters of a neural network
+    '''
+    
     dCdL1 =  y - y_
     dCdW1 =  dot(L0, dCdL1.T ) 
     
