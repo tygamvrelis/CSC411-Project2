c9f38647bdb27ef2c4b8654bc608a4dfc5e94429
zhan2487 <msnhaozhang@live.ca>
2018-02-17 15:50:55 -0500

    Copied all of part 8 onto file

--- Code/part8.py
+++ Code/part8.py
@@ -1,13 +1,14 @@
+import numpy as np
 from torch.autograd import Variable
 import torch
-import numpy as np
+
 import matplotlib.pyplot as plt
 
 from scipy.io import loadmat
 
 #matplotlib inline
 
-M = loadmat("mnist_all.mat")
+M = loadmat("../Data/mnist_all.mat")
 
 
 def get_test(M):
@@ -56,3 +57,29 @@ train_idx = np.random.permutation(range(train_x.shape[0]))[:1000]
 x = Variable(torch.from_numpy(train_x[train_idx]), requires_grad=False).type(dtype_float)
 y_classes = Variable(torch.from_numpy(np.argmax(train_y[train_idx], 1)), requires_grad=False).type(dtype_long)
 #################################################################################
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(dim_x, dim_h),
+    torch.nn.ReLU(),
+    torch.nn.Linear(dim_h, dim_out),
+)
+
+loss_fn = torch.nn.CrossEntropyLoss()
+learning_rate = 1e-2
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+for t in range(10000):
+    y_pred = model(x)
+    loss = loss_fn(y_pred, y_classes)
+    
+    model.zero_grad()  # Zero out the previous gradient computation
+    loss.backward()    # Compute the gradient
+    optimizer.step()   # Use the gradient information to 
+                       # make a step
+x = Variable(torch.from_numpy(test_x), requires_grad=False).type(dtype_float)
+y_pred = model(x).data.numpy()
+
+np.mean(np.argmax(y_pred, 1) == np.argmax(test_y, 1))
+model[0].weight
+model[0].weight.data.numpy()[10, :].shape
+plt.imshow(model[0].weight.data.numpy()[10, :].reshape((28, 28)), cmap=plt.cm.coolwarm)
+plt.imshow(model[0].weight.data.numpy()[12, :].reshape((28, 28)), cmap=plt.cm.coolwarm)
